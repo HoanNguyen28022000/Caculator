f460faaa654bb30d13b5ff9030c87f9e59503ba5
HoanNguyen28022000 <63435897+HoanNguyen28022000@users.noreply.github.com>
2020-10-05 22:15:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaCas;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
